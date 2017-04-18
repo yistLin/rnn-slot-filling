7705c76b1550e7984361693a8a092ec03f9227af
Yist Lin <yishen992@gmail.com>
2017-04-18 12:52:28 +0800

    use a strange method to fit model

--- birnn.py
+++ birnn.py
@@ -37,12 +37,14 @@ def train_model(datasets, buckets, from_size=1000, to_size=100):
         input_length = buckets[bucket_id]
         model.add(Embedding(from_size, embedding_size, input_length=input_length))
         model.add(Bidirectional(LSTM(embedding_size, return_sequences=True)))
-        model.add(Bidirectional(LSTM(embedding_size)))
-        model.add(Dense(to_size))
-        model.add(Activation('softmax'))
+        # model.add(Bidirectional(LSTM(embedding_size)))
+        model.add(LSTM(embedding_size))
+        model.add(Dense(64))
+        model.add(Dense(input_length))
+        model.add(Activation('relu'))
 
-        sgd = SGD(lr=0.1, decay=1e-5, momentum=0.9, nesterov=True)
-        model.compile(loss='sparse_categorical_crossentropy', optimizer=sgd)
+        # sgd = SGD(lr=0.1, decay=1e-5, momentum=0.9, nesterov=True)
+        model.compile(loss='categorical_crossentropy', optimizer='adam')
 
         print('model %d with input_length=%3d built' % (bucket_id, buckets[bucket_id]))
         models.append(model)
@@ -61,8 +63,8 @@ def train_model(datasets, buckets, from_size=1000, to_size=100):
 
 def main():
     buckets = [3, 6, 10, 20]
-    x_data, x_dict, _ = read_data(sys.argv[1])
-    y_data, _, y_dict = read_data(sys.argv[2])
+    x_data, x_word2id, _ = read_data(sys.argv[1])
+    y_data, y_word2id, y_dict = read_data(sys.argv[2])
 
     datasets = [[] for i in range(len(buckets))]
     for source_ids, target_ids in zip(x_data, y_data):
@@ -71,15 +73,15 @@ def main():
                 datasets[bucket_id].append((source_ids, target_ids))
                 break
 
-    from_size = len(x_dict)
-    to_size = len(y_dict)
+    from_size = len(x_word2id)
+    to_size = len(y_word2id)
     for bucket_id, bucket_size in enumerate(buckets):
         print('dataset[%d] with length = %d, has %d' % (bucket_id, bucket_size, len(datasets[bucket_id])))
 
         for data_id, (from_ids, to_ids) in enumerate(datasets[bucket_id]):
             # append _PAD
-            from_ids += [x_dict['_PAD']] * (bucket_size - len(from_ids))
-            to_ids += [x_dict['_PAD']] * (bucket_size - len(to_ids))
+            from_ids += [x_word2id['_PAD']] * (bucket_size - len(from_ids))
+            to_ids += [y_word2id['_PAD']] * (bucket_size - len(to_ids))
 
             # transform to one-hot
             # if embedding is used, we don't need to turn X_train to one-hot array
